838b8c5d400cc3a891069fa3861d6103e04d8776
TzlilLevi <57330791+TzlilLevi@users.noreply.github.com>
2022-04-17 17:31:42 +0300

    alert_center_view

--- frontend/src/components/goodMans.vue
+++ frontend/src/components/goodMans.vue
@@ -29,6 +29,7 @@
   background-image: url("https://images.pexels.com/photos/2030190/pexels-photo-2030190.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260");
   background-repeat: no-repeat;
   background-size: 1750px 750px;
+  filter: grayscale(75%);
   .text-box{
   display:flex;
   flex-direction:column;
